17e605ec2a28ea6a56ed53eeca9dc757c3affeb7
Jowayne Josephs <jowayne.josephs@gmail.com>
2017-10-26 18:53:40 -0600

    Add stuff

--- game.js
+++ game.js
@@ -1,9 +1,27 @@
 var Game = (function () {
-	
-	function Game() {
 
+	function Game(){
+		this.deck = new Deck(true)
+		this.player=new Player();
+		var player
+		
+		this.play = function() {
+			var cardDiv=document.getElementById('cardfield')
+			var play = document.getElementById('play')
+			deck = new Deck(true)
+			player = new Player()
+			
 	}
+	
+	function hold() {
+		
+	}
+	
+	function deal() {
+		deck.deal();
+	}
+}
 
 	return Game;
-	
+
 })();
--- player.js
+++ player.js
@@ -1,7 +1,11 @@
 var Player = (function () {
 	
 	function Player() {
+		this.account=1000;
+	}
 
+	function updateAccount(amount){
+		return amount
 	}
 
 	return Player;
